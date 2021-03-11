9aa9347c09a31f58a537722f29f8265cde455c2b
lunglagael <lunglagael@gmail.com>
2021-03-11 05:35:11 +0100

    Header and Assets

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import './Header.css'
+import TeslaLogo from '../assets/teslaLogoSmall.svg'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header_logo'>
+                <img src={TeslaLogo} alt='Tesla Logo'/>
+            </div>
+
+            <div className='header_center'>
+                <p>Model X</p>
+                <p>Model J</p>
+                <p>Model S</p>
+                <p>Model Y</p>
+                <p>Solar Roof</p>
+                <p>Solar Panel</p>
+            </div>
+
+            <div className='header_right'>
+                <p>Shop</p>
+                <p>Tesla Account</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
